c83743d5f5b91355a9efd1902ae6c8bfde4ecf69
christian-ritz <ritz@nexs.ku.dk>
2019-06-08 00:41:11 +0200

    Ensuring vector-valued output

---
+++ R/rse.R
@@ -0,0 +1,26 @@
+"rse" <- function(object, resvar = FALSE)
+{
+    if (!is.null(object$"objList"))
+    {
+        fitValue <- object$"minval"
+    } else {
+        fitValue <- as.vector(object[["fit"]][["value"]])
+    }
+
+    rse <- switch(object$"type",
+    "continuous" = fitValue / df.residual(object),
+    "binomial" = NA,
+    "Poisson" = NA,
+    "negbin1" = NA,
+    "negbin2" = NA,
+    "event" = NA,
+    "ssd" = NA,
+    "standard" = fitValue / df.residual(object))
+    
+    if (resvar) 
+    {
+        rse
+    } else {
+        sqrt(rse)
+    }
+}
